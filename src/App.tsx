df12686a360a900943788e921d6ba647c2b68b15
Alando S Almeida Sobrinho <alandoalmeida0803@7j50z3.onmicrosoft.com>
2023-07-20 18:25:55 -0300

    feat: estilização animação

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Feeds } from "./Feeds";
+
+function App() {
+  return (
+    <>
+      <Feeds />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Feeds/index.tsx
@@ -0,0 +1,75 @@
+import React from "react";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faFacebookF,
+  faInstagram,
+  faTwitter,
+  fab,
+} from "@fortawesome/free-brands-svg-icons";
+import {
+  faHeart,
+  faComment,
+  faShareFromSquare,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import styles from "./styles.module.css";
+import image from "../assets/eu.jpg";
+
+library.add(fab);
+
+export const Feeds: React.FC = () => {
+  return (
+    <>
+      <div className={styles.profileCard}>
+        <div className={styles.profileCardInner}>
+          <div className={styles.profileCardFront}>
+            <div className={styles.title}>
+              <h3>Rosilene M. Alves</h3>
+              <span>Web Developer , BRA</span>
+            </div>
+          </div>
+          <div className={styles.profileCardBack}>
+            <img src={image} alt="Image Rosilene M. Alves" />
+            <h3>Rosilene M. Alves</h3>
+            <span className={styles.job}>Web Developer</span>
+            <div className={styles.social}>
+              <span>
+                <FontAwesomeIcon
+                  className={styles.faBrands}
+                  icon={faInstagram}
+                />
+              </span>
+              <span>
+                <FontAwesomeIcon className={styles.faBrands} icon={faTwitter} />{" "}
+              </span>
+              <span>
+                <FontAwesomeIcon
+                  className={styles.faBrands}
+                  icon={faFacebookF}
+                />
+              </span>
+            </div>
+            <div className={styles.btns}>
+              <button type="button">Follow</button>
+              <button type="button">Message</button>
+            </div>
+            <div className={styles.actvity}>
+              <span>
+                <FontAwesomeIcon className="fa-regular" icon={faHeart} />
+              </span>
+              <span>
+                <FontAwesomeIcon className="fa-regular" icon={faComment} />
+              </span>
+              <span>
+                <FontAwesomeIcon
+                  className="fa-regular"
+                  icon={faShareFromSquare}
+                />
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
